058fe6293369ba02d8fb1577eadbe955dc227b06
tomb233 <thomasregan@me.com>
2015-11-09 12:29:59 +0000

    Version 1 of Node Class.

--- src/queuemanager/Node.java
+++ src/queuemanager/Node.java
@@ -2,6 +2,19 @@ package queuemanager;
 
 /**
  * Created by Tom on 09/11/2015.
+ * Used for implementing the LinkedLists
+ * Each node contains a reference to the next node in the list
  */
-public class Node {
+public class Node<T> {
+    private T item;
+    private T value;
+    private Node next;
+    public Node(T item,T value, Node next){
+        this.item = item;
+        this.value = value;
+        this.next = next;
+    }
+    public void printNode(){
+        System.out.println(value + " ");
+    }
 }
