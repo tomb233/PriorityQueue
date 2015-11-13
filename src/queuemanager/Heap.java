3e18a6d5ea067b8f6acdf54ef287d846334adea0
tomb233 <thomasregan@me.com>
2015-11-13 12:54:04 +0000

    Finished heap. Added all methods + comments

--- src/queuemanager/Heap.java
+++ src/queuemanager/Heap.java
@@ -1,47 +1,150 @@
 package queuemanager;
 
+
+
 /**
  * Created by Tom on 12/11/2015.
  */
+
 public class Heap<T> implements PriorityQueue<T> {
-    private static final int default_max_size = 10;
+
+    protected Object[] array;
     protected int size;
-    private Object[] heap;
 
-    @SuppressWarnings("unchecked")
+    /**
+     * Constructs a new BinaryHeap.
+     */
+
     public Heap(int s) {
-
-        heap = new Object[s];
+        array = new Object[s];
         size = 0;
     }
 
     @Override
-    public boolean isEmpty(){
-        return size <= -1;
+    public boolean isEmpty(){ //Returns true only if the array is empty
+        return size == 0;
     }
-
     @Override
     public T head() throws QueueUnderflowException {
-        return null;
+        if (isEmpty()) {
+            throw new QueueUnderflowException();
+        } else {
+            return ((Wrapper<T>) array[0]).getItem();
+        }
     }
 
     @Override
-    public void add(T item, int priority) throws QueueOverflowException {
+    public void remove() throws QueueUnderflowException{ //removes node
+        int index2 = size - 1;
+        swap(index2,0);
+        array[index2] = null;
+        size--;
+        bubbleDown();
+
+    }
+
+    @Override
+    public void add(T item, int priority) throws QueueOverflowException { //adds to heap
         size++;
-        int index = size;
-        heap[index] = new Wrapper<>(item, priority);;
+        int index = size-1;
+        array[index] = new Wrapper<>(item, priority);
+        bubbleUp();
+
+    }
+    protected void swap(int index1, int index2) { //swaps two nodes
+        Object tmp = array[index1];
+        Object tmp2 = array[index2];
+        array[index1] = tmp2;
+        array[index2] = tmp;
+        System.out.println("Moving " + array[index2] + " to position at " + array[index1]);
+    }
+
+    protected int parent(int i){ //returns parent index
+        return i/2;
+    }
+    protected int leftIndex(int i){ //returns index of a particular nodes left child
+        return (i * 2)+1;
+    }
+    protected boolean hasLeftChild(int i){ //return true if node has left child
+        return leftIndex(i) <= size;
+    }
+    protected boolean hasRightChild(int i){ //return true if node has right child
+        return rightIndex(i) <= size;
+    }
+    protected int rightIndex(int i){ //returns index of a particular nodes right child
+        return (i * 2) + 2;
+    }
+
+    protected void bubbleUp(){ //Sorts out the heap starting from the most recent insert. Mainly used when inserting into the heap
+        int index = size-1;
+        if(size > 1){
+
+            while(((Wrapper<T>)array[index]).getPriority() > ((Wrapper<T>)array[parent(index)]).getPriority()){
+                System.out.println("Now comparing the newest value and its parent");
+                System.out.println("Index value is"+((Wrapper<T>)array[index]).getPriority());
+                System.out.println("Parent index value is" + ((Wrapper<T>) array[parent(index)]).getPriority());
+                swap(index, parent(index));
+                System.out.println("Swapping");
+                index = parent(index);
+            }
+        }
     }
 
 
-    @Override
-    public void remove() throws QueueUnderflowException {
-
+    protected void bubbleDown() { //Sorts out heap starting from root. Mainly used when removing the root
+            int index = 0;
+            while (hasLeftChild(index)) {
+                if(((Wrapper<T>)array[leftIndex(index)]) == null || ((Wrapper<T>)array[rightIndex(index)]) == null) { //Error catching code, stops nullpointexceptions when working with array
+                    break;
+                }
+                int biggerChild = leftIndex(index); //Defaults largest child to left as we fill the heap left to right
+                if (hasRightChild(index) && ((Wrapper<T>)array[leftIndex(index)]).getPriority() < ((Wrapper<T>)array[rightIndex(index)]).getPriority()   ) { //Changes largest child only if right child is larger than left child.
+                    biggerChild = rightIndex(index);
+                    System.out.println("right index selected");
+                }
+                    int parent = ((Wrapper<T>)array[index]).getPriority();
+                    int child = ((Wrapper<T>)array[biggerChild]).getPriority();
+                    if (parent < child) { // Decides if child is bigger than parent, if so swaps places
+                        swap(index, biggerChild);
+                    }
+                    index = biggerChild;// updates index to the index of the largest child
+        }
     }
-
-
-
     @Override
     public String toString() {
+
+
+
+        int index = array.length - 1;
+
+        System.out.print("heapArray: ");    // array format
+        for(int m=0; m<=index; m++)
+            if(array[m] != null)
+                System.out.print( array[m] + " ");
+            else
+                System.out.print( "-- ");
+        System.out.println();
+        for(int x = 0;x <=index;x++){
+            if(hasLeftChild(x)){
+                System.out.println("Index is "+x);
+                System.out.print("Parent is ");
+                System.out.println(array[x]);
+                System.out.print("Left Child is ");
+                System.out.println(array[leftIndex(x)]);
+                if(rightIndex(x) < array.length){
+                    System.out.print("Right child is ");
+                    System.out.println(array[rightIndex(x)]);
+                }
+
+
+            }
+
+        }
         return null;
     }
+
+
+
+
+
 }
